4ee2aa921a2fe832f1d8f644bbe6995e57c7e912
cbergh <cbergh@datakitchen.io>
2013-09-01 22:51:08 -0400

    boto

--- second file 1.py
+++ second file 1.py
@@ -1,13 +1,36 @@
 __author__ = 'Chris Bergh'
 
 import boto
+import os
+import ConfigParser
+
+boto_path = os.path.expandvars("${BOTO_PATH}")
+boto_config = os.path.expandvars("${BOTO_CONFIG}")
+print ('BOTO_PATH ' + boto_path )
+print ('BOTO_CONFIG ' + boto_config )
+
+
+config = ConfigParser.ConfigParser()
+config.read(boto_config)
+
+print config.sections()
+
+access_key = config.get('Credentials','aws_access_key_id' )
+secret_key = config.get('Credentials','aws_secret_access_key')
+print access_key
+print secret_key
+
+from boto.s3.connection import S3Connection
+
+conn = S3Connection(access_key, secret_key)
+
+conn.create_bucket('testbotobucket2')
+
+print ('all done \n')
 
 
 
-print ('hello \n')
 
-print (2 **4)
 
-print ('hello again \n')
 
 
